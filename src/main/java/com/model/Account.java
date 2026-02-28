ef23406ca665557500907b1c905c19045dcaaeb5
agoodtaco <jpc21@email.sc.edu>
2026-02-28 01:29:24 -0500

    #12 Create Account Class Stub

---
+++ src/main/java/com/model/Account.java
@@ -0,0 +1,49 @@
+package com.model;
+
+import java.util.UUID;
+
+public class Account {
+    private UUID accountID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String username;
+    private String password;
+    private Role role;
+
+    public Account(UUID accountID, String user, String password) {
+        this.accountID = accountID;
+        this.username = user;
+        this.password = password;
+    }
+    
+    public boolean addEmail(String user, String password) {
+        return true;
+    }
+
+    public boolean addName(String firstName, String lastName) {
+        return true;
+    }
+
+    public boolean login() {
+        return true;
+    }
+    public boolean logout() {
+        return true;
+    }
+
+    public boolean removeUser(String user, String password) {
+        return true;
+    }
+
+    public boolean editUser(String user, String password) {
+        return true;
+    }
+
+    public boolean resetPassword(String user, String password) {
+        return true;
+    }
+
+    
+    
+}
