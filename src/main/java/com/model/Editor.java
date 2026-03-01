cd5d45774a509f78a0aca5960f9bb6e4e4756b32
colekenn <colekennedy56@gmail.com>
2026-03-01 16:44:30 -0500

    Create Editor Class Stub

---
+++ src/main/java/com/model/Editor.java
@@ -0,0 +1,47 @@
+package com.model;
+
+import java.util.UUID;
+
+public class Editor {
+    private boolean admin;
+
+    public Editor() {
+        this.admin = false;
+    }
+
+    public boolean createQuestion(Question question) {
+        return true;
+    }
+
+    public boolean editQuestion(Question question) {
+        return true;
+    }
+
+    public boolean createSolution(Question question, Solution solution) {
+        return true;
+    }
+
+    public boolean editSolution(Solution solution) {
+        return true;
+    }
+
+    public boolean approveQuestion(boolean admin, Question question) {
+        return true;
+    }
+
+    public boolean approveSolution(boolean admin, Solution solution) {
+        return true;
+    }
+
+    public boolean removeQuestion(boolean admin, Question question) {
+        return true;
+    }
+
+    public boolean removeSolution(boolean admin, Solution solution) {
+        return true;
+    }
+
+    public boolean addTrustedRole(boolean admin, UUID accountID, QuestionTag questionTag) {
+        return true;
+    }
+}
