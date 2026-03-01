f014443104be88ab1fb14e8793a9e22eadb5b28a
agoodtaco <jpc21@email.sc.edu>
2026-03-01 14:38:32 -0500

    #10 Comment Class Stub

--- src/main/java/com/model/Comment.java
+++ src/main/java/com/model/Comment.java
@@ -1,24 +1,41 @@
 package com.model;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.UUID;
 
 public class Comment {
     private String text;
     private UUID accountID;
-    private String datePosted;
     private ArrayList<Comment> replies;
     private int likes;
+    private String datePosted;
 
     public Comment(String text, UUID accountID, String datePosted) {
         this.text = text;
         this.accountID = accountID;
-        this.datePosted = datePosted;
+        this.datePosted = new Date().toString();
         this.replies = new ArrayList<>();
         this.likes = 0;
     }
 
-    public boolean reply(UUID accountID, String text, String datePosted) {
+    public String getText() {return text;}
+    public UUID getAccountID() {return accountID;}
+    public String datePosted() {return datePosted;}
+    public ArrayList<Comment> getReplies() {return replies;}
+    public int getLikes() {return likes;}
+
+    public void setText() {
+
+    }
+    public void setAccountID() {
+
+    }
+    public void setDatePosted() {
+
+    }
+
+    public boolean addReply(UUID accountID, String text, String datePosted) {
         return true;
     }
 
