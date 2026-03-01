f60e12a35624a7bdd10c3a2e2e659a6cf85d6e91
agoodtaco <jpc21@email.sc.edu>
2026-03-01 15:08:18 -0500

    #9 Create Solution Class Stub

---
+++ src/main/java/com/model/Solution.java
@@ -0,0 +1,66 @@
+package com.model;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Solution {
+    private UUID authorID;
+    private String title;
+    private Language language;
+    private ArrayList<Segment> segments;
+    private ArrayList<Comment> comments;
+    private int likes;
+    private boolean approved;
+
+    public Solution(UUID authorID, String title, Language language, ArrayList<Segment> segments, boolean approved) {
+        this.authorID = authorID;
+        this.title = title;
+        this.language = language;
+        this.segments = segments;
+        this.approved = approved;
+        this.likes = 0;
+        this.comments = new ArrayList<>();
+    }
+    public Solution(String title, Language language, ArrayList<Segment> segments, boolean approved) {
+        this.title = title;
+        this.language = language;
+        this.segments = segments;
+        this.approved = approved;
+        this.likes = 0;
+        this.comments = new ArrayList<>();
+    }
+
+    public UUID getAuthorID() {return authorID;}
+    public String getTitle() {return title;}
+    public Language getLanguage() {return language;}
+    public ArrayList<Segment> getSegments() {return segments;}
+    public ArrayList<Comment> getComments() {return comments;}
+    public int getLikes() {return likes;}
+    public boolean getApproved() {return approved;}
+
+    public void setAuthorID(UUID authorID) {
+
+    }
+    public void setTitle(String title) {
+
+    }
+    public void setLanguage(Language language) {
+
+    }
+    public void setSegments(ArrayList<Segment> segments) {
+
+    }
+    public void setApproved(boolean approved) {
+
+    }
+
+    public boolean submit() {
+        return true;
+    }
+
+    public void like() {
+
+    }
+
+    
+}
