11c199013d799b504cc9577a0b90701d319f7e1d
agoodtaco <jpc21@email.sc.edu>
2026-03-01 13:55:50 -0500

    #11 Create QuestionTag Class Stub

---
+++ src/main/java/com/model/questionTag.java
@@ -0,0 +1,31 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class questionTag {
+    private ArrayList<Category> category;
+    private ArrayList<Language> language;
+    private ArrayList<Course> course;
+
+    public questionTag(ArrayList<Category> category, ArrayList<Language> language, ArrayList<Course> course) {
+        this.category = category;
+        this.language = language;
+        this.course = course;
+    }
+
+    public ArrayList<Category> getCategory() {return category;}
+    public ArrayList<Language> getLanguage() {return language;}
+    public ArrayList<Course> getCourse() {return course;}
+    
+    public void setCateogry(ArrayList<Category> category) {
+
+    }
+
+    public void setLanguage(ArrayList<Language> language) {
+
+    }
+
+    public void setCourse(ArrayList<Course> course) {
+        
+    }
+}
