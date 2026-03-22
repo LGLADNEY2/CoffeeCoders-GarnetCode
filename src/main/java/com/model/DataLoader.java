b858511741326c5ca92cad85c607b3402a6058b1
ctytone <christyt1806@gmail.com>
2026-03-22 15:43:06 -0400

    #25 Fixed DataLoader Comment Bug

--- src/main/java/com/model/DataLoader.java
+++ src/main/java/com/model/DataLoader.java
@@ -8,9 +8,6 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import static com.model.DataType.HINT;
-import static com.model.DataType.STRING;
-
 public class DataLoader extends DataConstants {
     public static ArrayList<Account> getAccounts() {
         ArrayList<Account> accounts = new ArrayList<>();
@@ -127,64 +124,66 @@ public class DataLoader extends DataConstants {
             JSONParser parser = new JSONParser();
             JSONArray questionsJSON = (JSONArray) parser.parse(reader);
 
-            for (int i = 0; i < questionsJSON.size(); i++) {
-                JSONObject questionJSON = (JSONObject) questionsJSON.get(i);
+            for (Object questionObj : questionsJSON) {
+                JSONObject questionJSON = (JSONObject) questionObj;
 
-                // Skip blank placeholder entries
                 String questionIDStr = (String) questionJSON.get(QUESTION_ID);
-                if (questionIDStr == null || questionIDStr.isEmpty()) continue;
-
+                String authorIDStr = (String) questionJSON.get(AUTHOR_ID);
+               
                 UUID questionID = UUID.fromString(questionIDStr);
-                UUID authorID = UUID.fromString((String) questionJSON.get(AUTHOR_ID));
+                UUID authorID = UUID.fromString(authorIDStr);
+
                 String title = (String) questionJSON.get(QUESTION_TITLE);
-                int rating = ((Long) questionJSON.get(QUESTION_RATING)).intValue();
-                int totalRatings = ((Long) questionJSON.get(QUESTION_TOTAL_RATINGS)).intValue();
-                ArrayList<Comment> comments = new ArrayList<>();
                 String datePosted = (String) questionJSON.get(QUESTION_DATE_POSTED);
                 Difficulty difficulty = Difficulty.valueOf((String) questionJSON.get(QUESTION_DIFFICULTY));
-                
-                Object recTimeObj = questionJSON.get(QUESTION_RECOMMENDED_TIME);
-                int recommendedTime;
-                if (recTimeObj instanceof Long) {
-                    recommendedTime = ((Long) recTimeObj).intValue();
-                } else {
-                    recommendedTime = Integer.parseInt((String) recTimeObj);
-                }
 
-                
-                // ================ segments ==================== //
-                // make a json array of the segments of each question, which is title, desc, data type, and data
-                // each "segments" instance is an array of these 4
+                int rating = ((Integer) questionJSON.get(QUESTION_RATING));
+                int totalRatings = ((Integer) questionJSON.get(QUESTION_TOTAL_RATINGS));
+                int recommendedTime = ((Integer) questionJSON.get(QUESTION_RECOMMENDED_TIME));
+
                 JSONArray segmentsJSON = (JSONArray) questionJSON.get(QUESTION_SEGMENTS);
                 ArrayList<Segment> segments = new ArrayList<>();
-                for (Object segObj : segmentsJSON) {
-                    JSONObject segJSON = (JSONObject) segObj;
-                    segments.add(new Segment(
-                        (String) segJSON.get(SEGMENT_TITLE),
-                        (String) segJSON.get(SEGMENT_DESC),
-                        DataType.valueOf((String) segJSON.get(SEGMENT_DATA_TYPE)),
-                        (String) segJSON.get(SEGMENT_DATA)
-                    ));
+                if (segmentsJSON != null) {
+                    for (Object segObj : segmentsJSON) {
+                        JSONObject segJSON = (JSONObject) segObj;
+                        segments.add(new Segment(
+                            (String) segJSON.get(SEGMENT_TITLE),
+                            (String) segJSON.get(SEGMENT_DESC),
+                            DataType.valueOf((String) segJSON.get(SEGMENT_DATA_TYPE)),
+                            (String) segJSON.get(SEGMENT_DATA)
+                        ));
+                    }
                 }
 
-                // ======================= question tags ======================= //
+                JSONObject tagJSON = (JSONObject) questionJSON.get(QUESTION_QUESTION_TAG);
                 ArrayList<Category> categories = new ArrayList<>();
-                for (Object catObj : (JSONArray) tagJSON.get(QUESTION_TAG_CATEGORY)) {
-                }
                 ArrayList<Language> languages = new ArrayList<>();
-                for (Object langObj : (JSONArray) tagJSON.get(QUESTION_TAG_LANGUAGE)) {
-                    languages.add(Language.valueOf((String) langObj));
-                }
                 ArrayList<Course> courses = new ArrayList<>();
-                for (Object courseObj : (JSONArray) tagJSON.get(QUESTION_TAG_COURSE)) {
-                    courses.add(Course.valueOf((String) courseObj));
+                if (tagJSON != null) {
+                    JSONArray categoriesJSON = (JSONArray) tagJSON.get(QUESTION_TAG_CATEGORY);
+                    if (categoriesJSON != null) {
+                        for (Object catObj : categoriesJSON) {
+                            categories.add(Category.valueOf((String) catObj));
+                        }
+                    }
+
+                    JSONArray languagesJSON = (JSONArray) tagJSON.get(QUESTION_TAG_LANGUAGE);
+                    if (languagesJSON != null) {
+                        for (Object langObj : languagesJSON) {
+                            languages.add(Language.valueOf((String) langObj));
+                        }
+                    }
+
+                    JSONArray coursesJSON = (JSONArray) tagJSON.get(QUESTION_TAG_COURSE);
+                    if (coursesJSON != null) {
+                        for (Object courseObj : coursesJSON) {
+                            courses.add(Course.valueOf((String) courseObj));
+                        }
+                    }
                 }
                 QuestionTag questionTag = new QuestionTag(categories, languages, courses);
 
-                // ===================== hints ===================== //
-                // make json array of hints
                 JSONArray hintsJSON = (JSONArray) questionJSON.get(QUESTION_HINTS);
-                // make array list of segments for hints, hint 
                 ArrayList<Segment> hints = new ArrayList<>();
                 if (hintsJSON != null) {
                     for (Object hintObj : hintsJSON) {
@@ -196,69 +195,58 @@ public class DataLoader extends DataConstants {
                             (String) hintJSON.get(SEGMENT_DATA)
                         ));
                     }
-                for (Object hintObj : hintsJSON) {
-                    hints.add(new Segment("Hint", (String) hintObj, "STRING", ""));
                 }
-            
 
-                // ================ solutions ================== //
-                // make json array
                 JSONArray solutionsJSON = (JSONArray) questionJSON.get(QUESTION_SOLUTIONS);
-                // make an empty array list of solutions that will be returned at the end
                 ArrayList<Solution> solutions = new ArrayList<>();
-                // if the json file has at least questions in it, continue
                 if (solutionsJSON != null) {
-                    // for every json object in the solution json jsonarray,
                     for (Object solObj : solutionsJSON) {
-                        // get all of the variables
                         JSONObject solJSON = (JSONObject) solObj;
                         String solAuthorIDStr = (String) solJSON.get(SOLUTION_AUTHOR_ID);
-                        if (solAuthorIDStr == null || solAuthorIDStr.isEmpty()) continue;
+                        String solLanguageStr = (String) solJSON.get(SOLUTION_LANGUAGE);
+
                         UUID solAuthorID = UUID.fromString(solAuthorIDStr);
                         String solTitle = (String) solJSON.get(SOLUTION_TITLE);
-                        Language solLanguage = Language.valueOf((String) solJSON.get(SOLUTION_LANGUAGE));
+                        Language solLanguage = Language.valueOf(solLanguageStr);
+
                         JSONArray solSegmentsJSON = (JSONArray) solJSON.get(SOLUTION_SEGMENTS);
-                        // make empty segement arraylist to hold the specific soltuions segments
                         ArrayList<Segment> solSegments = new ArrayList<>();
-                        for (Object ssObj : solSegmentsJSON) {
-                            JSONObject ssJSON = (JSONObject) ssObj;
-                            solSegments.add(new Segment(
-                                (String) ssJSON.get(SEGMENT_TITLE),
-                                (String) ssJSON.get(SEGMENT_DESC),
-                                DataType.valueOf((String) ssJSON.get(SEGMENT_DATA_TYPE)),
-                                (String) ssJSON.get(SEGMENT_DATA)
-                            ));
-                        }
-                        ArrayList<Comment> solComments = new ArrayList<>();
-
-                        Object likesObj = solJSON.get(SOLUTION_LIKES);
-                        int likes = 0;
-                        if (likesObj instanceof Long) {
-                            likes = ((Long) likesObj).intValue();
+                        if (solSegmentsJSON != null) {
+                            for (Object ssObj : solSegmentsJSON) {
+                                JSONObject ssJSON = (JSONObject) ssObj;
+                                solSegments.add(new Segment(
+                                    (String) ssJSON.get(SEGMENT_TITLE),
+                                    (String) ssJSON.get(SEGMENT_DESC),
+                                    DataType.valueOf((String) ssJSON.get(SEGMENT_DATA_TYPE)),
+                                    (String) ssJSON.get(SEGMENT_DATA)
+                                ));
+                            }
                         }
 
-
-                        Object approvedObj = solJSON.get(SOLUTION_APPROVED);
-                        boolean approved = (approvedObj instanceof Boolean) && (Boolean) approvedObj;
-                        //do we need approved boolean?
-                        // solutions.add(solAuthorID, solTitle, solLanguage, solSegments, solComments, likes, approved));
-                        solutions.add(new Solution(solAuthorID, solTitle, solLanguage, solSegments, solComments, likes, approved));
+                        ArrayList<Comment> solComments = parseComments((JSONArray) solJSON.get(SOLUTION_COMMENTS));
+                        int likes = ((Integer) solJSON.get(SOLUTION_LIKES));
+                        //todo maaybe remove approved bool
+                        solutions.add(new Solution(solAuthorID, solTitle, solLanguage, solSegments, solComments, likes));
                     }
                 }
 
-                questions.add(new Question(questionID, authorID, title, datePosted, rating, totalRatings, recommendedTime, difficulty, segments, questionTag, hints, solutions, comments));
-                // ======== comments ========== //
-                // make empty array list of comments 
-                ArrayList<Comment> comments = new ArrayList<>();
-                String commentText = (String) questionJSON.get(COMMENT_TEXT);
-                UUID commentAccountID = (UUID) questionJSON.get(COMMENT_ACCOUNT_ID);
-                int commentLikes = (Integer) questionJSON.get(COMMENT_LIKES);
-                String commentDatePosted = (String) questionJSON.get(COMMENT_DATE_POSTED);
+                ArrayList<Comment> comments = parseComments((JSONArray) questionJSON.get(QUESTION_COMMENTS));
 
-                // comments.add(new Comment(commentText, commentAccountID,));
-
-                questions.add(new Question(questionID, authorID, title, rating, datePosted,
-                recommendedTime, difficulty, segments, questionTag, hints, solutions, comments));
+                questions.add(new Question(
+                    questionID,
+                    authorID,
+                    title,
+                    datePosted,
+                    rating,
+                    totalRatings,
+                    recommendedTime,
+                    difficulty,
+                    segments,
+                    questionTag,
+                    hints,
+                    solutions,
+                    comments
+                ));
             }
 
             reader.close();
@@ -267,6 +255,26 @@ public class DataLoader extends DataConstants {
         }
 
         return questions;
+    }
+
+    private static ArrayList<Comment> parseComments(JSONArray commentsJSON) {
+        ArrayList<Comment> comments = new ArrayList<>();
+        if (commentsJSON == null) {
+            return comments;
         }
+
+        for (Object commentObj : commentsJSON) {
+            JSONObject commentJSON = (JSONObject) commentObj;
+            String text = (String) commentJSON.get(COMMENT_TEXT);
+            String accountIDStr = (String) commentJSON.get(COMMENT_ACCOUNT_ID);
+
+            UUID accountID = UUID.fromString(accountIDStr);
+            String datePosted = (String) commentJSON.get(COMMENT_DATE_POSTED);
+            int likes = ((Integer) commentJSON.get(COMMENT_LIKES));
+            ArrayList<Comment> replies = parseComments((JSONArray) commentJSON.get(COMMENT_REPLIES));
+            comments.add(new Comment(text, accountID, replies, likes, datePosted));
+        }
+
+        return comments;
     }
 }
