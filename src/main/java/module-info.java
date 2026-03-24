fa585f72890e828b90d15473e2656fdd3b665b37
agoodtaco <jpc21@email.sc.edu>
2026-03-24 12:47:45 -0400

    Added tests

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module com.techprep {
     requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
+    requires junit;
 
     opens com.techprep to javafx.fxml;
     exports com.techprep;
+
+    opens com.model to javafx.fxml;
     exports com.model;
 }
